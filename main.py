0aaa7e1d49b817f258ff54b668d5efd11d5f3e06
GarrisonTravis <58957083+GarrisonTravis@users.noreply.github.com>
2021-10-11 18:28:34 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -2,31 +2,36 @@ from chaining import ChainingHashTable
 from probing import ProbingHashTable
 from double import DoubleHashTable
 
-cht = ChainingHashTable(0, 50)
-pht = ProbingHashTable(0, 54)
-dht = DoubleHashTable(0, 54)
-
 # Open the file
-inFile = open('Dictionary.txt', 'r')
+val = input("Do you have your own file? (y/n): ")
+if val == 'y' or val == 'Y' or val == 'yes' or val == 'Yes' or val == 'YES':
+    fileName = input("Enter file name (include path if not in this directory): ")
+
+    inFile = open(fileName, 'r')
+else :
+    inFile = open('Dictionary.txt', 'r')
+
+# Print Selection Menu
+print("\n1. Separate Chaining")
+print("2. Linear Probing")
+print("3. Double Hashing")
+choice = input("Please select a hashing method from above: ")
+
+# Make instance of the hash table
+if choice == '3':
+    ht = DoubleHashTable(0, 10000)
+elif choice == '2':
+    ht = ProbingHashTable(0, 10000)
+else:
+    ht = ChainingHashTable(0, 10000)
 
 # Insert each line into the hash table
 for line in inFile:
-    cht.insert(line.rstrip())
-    pht.insert(line.rstrip())
-    dht.insert(line.rstrip())
+    ht.insert(line.rstrip())
 
 # Close the file
 inFile.close()
 
 # Print all the unique words in the file and the number of times they appear
-print("CHT:")
-cht.printAll()
-print(cht.size)
-
-print("\nPHT:")
-pht.printAll()
-print(pht.size)
-
-print("\nDHT:")
-dht.printAll()
-print(dht.size)
+print("\nThere are " + str(ht.size) + " unique words in the file:")
+ht.printAll()
